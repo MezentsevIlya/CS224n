5cc7832af42e51cf10a76d00d36fbaadbd501450
Илья Мезенцев <ilyamezentsev@MacBook-Pro-Ila.local>
2019-06-29 22:17:41 +0500

    implement a4 (a)

--- winter_2019/a4/utils.py
+++ winter_2019/a4/utils.py
@@ -29,8 +29,13 @@ def pad_sents(sents, pad_token):
     sents_padded = []
 
     ### YOUR CODE HERE (~6 Lines)
-
-
+    max_len = max(len(sent) for sent in sents)
+    for sent in sents:
+        l = len(sent)
+        if l < max_len:
+            sents_padded.append(sent + [pad_token] * (max_len - l))
+        else:
+            sents_padded.append(sent)
     ### END YOUR CODE
 
     return sents_padded
